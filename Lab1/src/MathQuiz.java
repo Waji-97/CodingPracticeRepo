c5f8075d4c2177fd1e831a7ff928b7ce390dfdcc
Waji <wajiwos16@gmail.com>
2022-06-22 16:06:25 +0900

    initial commit

---
+++ Lab1/src/MathQuiz.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class MathQuiz {
+	public static void main(String[] args) {
+		AdditionProblem	additionProblem = new AdditionProblem();
+		String num = additionProblem.getProblem();
+		System.out.println("Question: " + num);
+		Scanner ans = new Scanner(System.in);
+		System.out.println("Your answer: ");
+		int uAns = ans.nextInt();
+		int sum = additionProblem.getAnswer();
+		
+		if (uAns == sum){
+			System.out.println("Your answer is correct!");
+		}
+		else{
+			System.out.println("Wrong answer!");
+		}
+		
+	      
+	      
+	}
+}
